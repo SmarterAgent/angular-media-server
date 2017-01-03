96a9ec0e0b460c53425e5a9d330d00e7976f4cb6
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2017-01-03 16:15:28 -0300

    Creates separate directive for setting background image

--- angular-media-server.js
+++ angular-media-server.js
@@ -2,6 +2,7 @@
   'use strict';
 
   angular.module('angular-media-server', ['angular-preload-image']);
+
   angular.module('angular-media-server').factory('utils', [function () {
     var utils = {
       bgImageCSS: bgImageCSS,
@@ -12,14 +13,51 @@
     function bgImageCSS(imageURL) {
       return { 'background-image': 'url("' + imageURL + '")' };
     }
-  }])
-  angular.module('angular-media-server').directive('mediaServerUrl', ['preLoader', 'utils', function (preLoader, utils) {
+  }]);
+
+  angular.module('angular-media-server').directive('mediaServerUrl', ['preLoader', function (preLoader) {
     return {
       restrict: 'A',
       terminal: true,
       priority: 100,
       link: function (scope, element, attrs) {
         var mediaServerURL = attrs.mediaServerUrl || "https://media.smarteragent.com/unsafe/";
+        var defaultImage = attrs.defaultImage;
+        var standByMediaServerURL = attrs.standByMediaServerUrl;
+
+        attrs.$observe('ngSrc', function () {
+          if (attrs.ngSrc) {
+            var image = attrs.ngSrc;
+
+            if (standByMediaServerURL) {
+              attrs.$set('src', standByMediaServerURL + image);
+
+              preLoader(
+                mediaServerURL + image,
+                function () {
+                  attrs.$set('src', mediaServerURL + image);
+                },
+                function () {
+                  if (defaultImage) attrs.$set('src', defaultImage);
+                }
+                )
+            } else {
+              attrs.$set('src', mediaServerURL + image);
+            }
+          } else {
+            if (defaultImage) attrs.$set('src', defaultImage);
+          }
+          
+        })
+      }
+    }
+  }]);
+
+  angular.module('angular-media-server').directive('bgMediaServerUrl', ['preLoader', 'utils', function (preLoader, utils) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var mediaServerURL = attrs.bgMediaServerUrl || "https://media.smarteragent.com/unsafe/";
         var defaultImage = attrs.defaultImage;
         var standByMediaServerURL = attrs.standByMediaServerUrl;
 
@@ -31,46 +69,21 @@
               element.css(utils.bgImageCSS(standByMediaServerURL + bgImage));
 
               preLoader(
-                mediaServerUrl + bgImage, 
+                mediaServerURL + bgImage, 
                 function () {
-                  element.css(utils.bgImageCSS(mediaServerUrl + bgImage));
+                  element.css(utils.bgImageCSS(mediaServerURL + bgImage));
                 },
                 function () {
                   if (defaultImage) element.css(utils.bgImageCSS(defaultImage));
                 })
             } else {
-              element.css(utils.bgImageCSS(mediaServerUrl + bgImage))
+              element.css(utils.bgImageCSS(mediaServerURL + bgImage))
             }
           } else {
             if (defaultImage) element.css(utils.bgImageCSS(defaultImage));
           }
         })
-
-        attrs.$observe('ngSrc', function () {
-          if (attrs.ngSrc) {
-            var image = attrs.ngSrc;
-
-            if (standByMediaServerURL) {
-              attrs.$set('src', standByMediaServerURL + image);
-
-              preLoader(
-                mediaServerUrl + image,
-                function () {
-                  attrs.$set('src', mediaServerUrl + image);
-                },
-                function () {
-                  if (defaultImage) attrs.$set('src', defaultImage);
-                }
-                )
-            } else {
-              attrs.$set('src', mediaServerUrl + image);
-            }
-          } else {
-            if (defaultImage) attrs.$set('src', defaultImage);
-          }
-          
-        })
       }
     }
-  }])
+  }]);
 })
